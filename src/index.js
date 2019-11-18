73e6a3836fd31d2135eab1c8f30e81ffa5934be2
Angel Mendez <angelmendezz751@gmail.com>
2019-11-17 21:27:42 -0600

    ADD measurementId TO FIREBASE INITIALIZE APP

--- src/index.js
+++ src/index.js
@@ -39,7 +39,8 @@ function Index({ features, children }) {
           projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
           storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
           messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.GATSBY_FIREBASE_APP_ID
+          appId: process.env.GATSBY_FIREBASE_APP_ID,
+          measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID
         });
         firebaseInstance
           .firestore()
